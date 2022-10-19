7219b4a9ff77711dd3731902946488cbbc97d626
FudgyDRS <31002491+FudgyDRS@users.noreply.github.com>
2022-10-19 02:20:53 -0700

    Add files via upload

---
+++ precache-manifest.b58b5c0ad750cbcc742b1573935fbb98.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f98937a31731375e64565dba0e55b1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e088df7ced5a4881205c",
+    "url": "/static/css/3.5f1fb496.chunk.css"
+  },
+  {
+    "revision": "2e1c6f190af2068796e5",
+    "url": "/static/css/main.bd8f3866.chunk.css"
+  },
+  {
+    "revision": "cf201d32e7790dc8d6f1",
+    "url": "/static/js/0.b54621db.chunk.js"
+  },
+  {
+    "revision": "e088df7ced5a4881205c",
+    "url": "/static/js/3.bb917b97.chunk.js"
+  },
+  {
+    "revision": "3aa57433bd1e7447b87b9e342f1260ae",
+    "url": "/static/js/3.bb917b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd0bce943190b1f987b",
+    "url": "/static/js/4.43f7884a.chunk.js"
+  },
+  {
+    "revision": "aefc06c8d6815bccc9c89204a9801f86",
+    "url": "/static/js/4.43f7884a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac97b6a4cb245cb1392",
+    "url": "/static/js/5.0f8cb3cf.chunk.js"
+  },
+  {
+    "revision": "4ebaad50e67278258416",
+    "url": "/static/js/6.1208ec41.chunk.js"
+  },
+  {
+    "revision": "436d5ccb6aebf66d7e9d",
+    "url": "/static/js/7.44344083.chunk.js"
+  },
+  {
+    "revision": "2e1c6f190af2068796e5",
+    "url": "/static/js/main.077462d2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.077462d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7e823e3407e332cf779",
+    "url": "/static/js/runtime-main.9adc5af8.js"
+  },
+  {
+    "revision": "2aa3269d3f3cc5c46e26c209bc2845fa",
+    "url": "/static/media/BtnFlipConnect.2aa3269d.svg"
+  },
+  {
+    "revision": "a243802d3627d101b3859e83b9b42030",
+    "url": "/static/media/DI_icon.a243802d.svg"
+  },
+  {
+    "revision": "1556c0b116cebf37b6335b043706f239",
+    "url": "/static/media/IN_icon.1556c0b1.svg"
+  },
+  {
+    "revision": "41b148b8b70d1e83cee492d5b224055c",
+    "url": "/static/media/ME_icon.41b148b8.svg"
+  },
+  {
+    "revision": "2d7a0dfcccf9de91fd557f2cc9bd9565",
+    "url": "/static/media/Peculiar-Pugs-Logo-300x300.2d7a0dfc.png"
+  },
+  {
+    "revision": "3622f8d7161f28a6053da1c9b80fca26",
+    "url": "/static/media/TG_icon.3622f8d7.svg"
+  },
+  {
+    "revision": "d7e9032d7e2ba8da3033f5b247c957d9",
+    "url": "/static/media/TW_icon.d7e9032d.svg"
+  },
+  {
+    "revision": "eb7dd9e48ec3f9c8d61f1592f3e52489",
+    "url": "/static/media/YT_icon.eb7dd9e4.svg"
+  },
+  {
+    "revision": "7e9c7c5b3f7ba9cea70fe80aeeae2adb",
+    "url": "/static/media/bg.7e9c7c5b.jpg"
+  },
+  {
+    "revision": "74dd625db0fab8e9d17468ef407a3ee3",
+    "url": "/static/media/btnConnect.74dd625d.svg"
+  },
+  {
+    "revision": "e530d4d0339500048b166800d640d9ea",
+    "url": "/static/media/btnLargeOff.e530d4d0.svg"
+  },
+  {
+    "revision": "e3b46221a376d4fa9ae028842872a0a1",
+    "url": "/static/media/btnLargeOn.e3b46221.svg"
+  },
+  {
+    "revision": "5e363d54c00301b6a557d91fc3ffcf0b",
+    "url": "/static/media/chevronDown.5e363d54.svg"
+  }
+]);
