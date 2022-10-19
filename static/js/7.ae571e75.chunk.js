b1c92834b4bb61b59d882b6a8e826d51f83471a5
FudgyDRS <31002491+FudgyDRS@users.noreply.github.com>
2022-10-19 02:18:02 -0700

    Delete static directory

--- static/js/7.ae571e75.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpneed-a-web-developer-21043817"]=this["webpackJsonpneed-a-web-developer-21043817"]||[]).push([[7],{966:function(e,p){}}]);
-//# sourceMappingURL=7.ae571e75.chunk.js.map
